26955fbd582a25d7f5ad34faccdf707945f175c4
x0ba <dax@omg.lol>
2024-02-21 08:19:27 -0800

    fix

--- pkgs/helm-ls/default.nix
+++ pkgs/helm-ls/default.nix
@@ -1,20 +1,19 @@
-{ buildGoModule
-, callPackage
-, lib
-,
-}:
-let
-  nvfetcher = (callPackage ../../_sources/go/generated.nix { }).helm-ls;
+{
+  buildGoModule,
+  callPackage,
+  lib,
+}: let
+  nvfetcher = (callPackage ../../_sources/go/generated.nix {}).helm-ls;
 in
-buildGoModule {
-  inherit (nvfetcher) pname version src;
+  buildGoModule {
+    inherit (nvfetcher) pname version src;
 
-  vendorHash = "sha256-KyeXVS07+WUV+Q5Z9Q+SFp/q9KLLQBpuBH9kRSAh8qg=";
+    vendorHash = "sha256-8mSX7fwgxwZ8aIXfv3WxLiVH5PjSFzcxM0oekod84tA=";
 
-  meta = with lib; {
-    description = "helm language server";
-    homepage = "https://github.com/mrjosh/helm-ls";
-    license = licenses.mit;
-    platforms = platforms.unix;
-  };
-}
+    meta = with lib; {
+      description = "helm language server";
+      homepage = "https://github.com/mrjosh/helm-ls";
+      license = licenses.mit;
+      platforms = platforms.unix;
+    };
+  }
